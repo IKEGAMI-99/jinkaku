3ca56c6fd2dd0e7b215a37f0cd37699d1f8b505a
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-08 18:44:38 +0900

    Show inference backend controls

--- app/src/main/java/com/ikegami99/jinkaku/MainActivity.kt
+++ app/src/main/java/com/ikegami99/jinkaku/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ikegami99.jinkaku.ui.BackendControlOverlay
 import com.ikegami99.jinkaku.ui.ModernJinkakuApp
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +17,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val vm: JinkakuViewModel = viewModel(factory = JinkakuViewModel.factory(application))
-            ModernJinkakuApp(vm)
+            Box(Modifier.fillMaxSize()) {
+                ModernJinkakuApp(vm)
+                BackendControlOverlay()
+            }
         }
     }
 }
