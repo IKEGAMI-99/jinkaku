9171b3a4b1c56b7f52a309f659daf152b456fe1d
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-08 19:48:25 +0900

    Remove floating backend control overlay

--- app/src/main/java/com/ikegami99/jinkaku/MainActivity.kt
+++ app/src/main/java/com/ikegami99/jinkaku/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.ikegami99.jinkaku.ui.BackendControlOverlay
 import com.ikegami99.jinkaku.ui.ModernJinkakuApp
 
 class MainActivity : ComponentActivity() {
@@ -17,10 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val vm: JinkakuViewModel = viewModel(factory = JinkakuViewModel.factory(application))
-            Box(Modifier.fillMaxSize()) {
-                ModernJinkakuApp(vm)
-                BackendControlOverlay()
-            }
+            ModernJinkakuApp(vm)
         }
     }
 }
